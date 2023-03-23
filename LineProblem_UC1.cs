5af2d50537eda6b73431cf2800b304053957aafc
shivamtr21 <shivamtripathi26297@gmail.com>
2023-03-23 13:52:39 +0530

    Line Comparision problem using object

---
+++ LineProblem_UC1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineProblemObject
+{
+    public class LineProblem_UC1
+    {
+        int x1, x2, y1, y2;
+        double Length;
+        
+        public void SolveTheProblem()
+        {
+            Console.WriteLine("Enter the x-cordinate of point 1 : x1");
+            x1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the x-cordinate of point 2 : x2");
+            x2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the y-cordinate of point 1 : y1");
+            y1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the y-cordinate of point 2 : y2");
+            y2 = Convert.ToInt32(Console.ReadLine());
+
+            Length = Math.Sqrt(((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)));
+
+            Console.WriteLine("Distance between two points: " + "(" + x1 + "," + y1 + ")" + "(" + x2 + "," + y2 + ") is :" + Length);
+
+        }
+    }
+}
