86e3773caacd670aad9050272df1256585af9adf
shivamtr21 <shivamtripathi26297@gmail.com>
2023-03-23 13:51:18 +0530

    Line Comparision problem using object

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace LineProblemObject
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*LineProblem_UC1 UC1 = new LineProblem_UC1();
+            UC1.SolveTheProblem();*/
+
+
+            /*LineProblem_UC2 UC2 = new LineProblem_UC2();
+            UC2.SolveTheProblem();*/
+
+            LineProblem_UC3 UC3 = new LineProblem_UC3();
+            UC3.SolveTheProblem();
+
+
+
+        }
+    }
+}
